b8b8c7babb22e19a9c48ce91f4e14d48807c6bc0
Trung0btCode <cutrung2k4@gmail.com>
2026-03-20 21:03:17 +0700

    Khoi tao du an

--- src/test/java/laptrinhJ2EE/demotravisci/CalculatorTest.java
+++ src/test/java/laptrinhJ2EE/demotravisci/CalculatorTest.java
@@ -10,6 +10,6 @@ public class CalculatorTest {
     public void testSum() {
         Calculator calculator = new Calculator();
         int result = calculator.sum(2, 3);
-        assertEquals(5, result);
+        assertEquals(6, result);
     }
 }
